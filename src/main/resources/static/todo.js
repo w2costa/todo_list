225122a640c674cd873892632dabc903b5b2217d
Wilson Wolf Costa <wilson.w.costa@gmail.com>
2026-05-26 09:58:12 -0300

    Commit final

--- src/main/resources/static/todo.js
+++ src/main/resources/static/todo.js
@@ -5,6 +5,23 @@ $(function () {
 
     var $lastClicked;
 
+    function updateTarefa(text, id) {
+        // $.post(server + "/tarefas", { tarefa_id: id, texto: text });
+        // console.log("Updating tarefa with id:", id, "and text:", text);
+        var tarefaData = { texto: text };
+        $.ajax({
+            url: server + "/tarefas/" + id,
+            method: "PUT",
+            contentType: "application/json; charset=UTF-8",
+            data: JSON.stringify(tarefaData),
+            dataType: "json",
+            success: function (response) {
+                // Tarefa atualizada com sucesso
+                console.log('Item updated successfully:', response);
+            }
+        });
+    }
+
     function newTarefa(text, $div) {
         // $.post(server + "/tarefa",
         //     {
@@ -67,12 +84,33 @@ $(function () {
         $(this).parent('.tarefa-item')
             .off('click')
             .hide('slow', function () {
+                $this = $(this);
+
+                // $.post(server + "/tarefa",
+                //     {
+                //         usuario: meu_login,
+                //         tarefa_id: $this.children(".tarefa-id").text(),
+                //         _method: "DELETE"
+                //     });
+
+                $.ajax({
+                    url: server + "/tarefas/" + $this.children(".tarefa-id").text(),
+                    method: "DELETE",
+                    dataType: "json",
+                    success: function (response) {
+                        // Tarefa deletada com sucesso
+                        console.log('Item deleted successfully:', response);
+                    }
+                });
+
                 $(this).remove();
             });
     }
 
     function savePendingEdition($tarefa) {
         var text = $tarefa.children('.tarefa-edit').val();
+        var id = $tarefa.children('.tarefa-id').text();
+        console.log("Saving tarefa with id:", id, "and text:", text);
         $tarefa.empty();
 
         // substitui o código comentado pelo código a seguir para evitar a vulnerabilidade de XSS
@@ -80,13 +118,19 @@ $(function () {
         //     .append("<div class='tarefa-delete'></div>")
         //     .append("<div class='clear'></div>");
 
-        $tarefa.append($("<div />")
-            .addClass("tarefa-texto")
-            .text(text))
+        $tarefa
+            .append($("<div />")
+                .addClass("tarefa-id")
+                .text(id))
+            .append($("<div />")
+                .addClass("tarefa-texto")
+                .text(text))
             .append($("<div />")
                 .addClass("tarefa-delete"))
             .append($("<div />")
                 .addClass("clear"));
+
+        updateTarefa(text, id);
 
         $(".tarefa-delete").click(onTarefaDeleteClick);
 
@@ -109,7 +153,7 @@ $(function () {
             $lastClicked = $(this);
 
             var text = $lastClicked.children('.tarefa-texto').text();
-
+            var id = $lastClicked.children('.tarefa-id').text();
             // substitui o código comentado pelo código a seguir para evitar a vulnerabilidade de XSS
             // var html = "<input type='text' " +
             //     "class='tarefa-edit' value='" +
@@ -117,6 +161,9 @@ $(function () {
             // $lastClicked.html(html);
 
             $lastClicked.empty()
+                .append($("<div />")
+                    .addClass("tarefa-id")
+                    .text(id))
                 .append($("<input />")
                     .attr("type", "text")
                     .addClass("tarefa-edit")
@@ -130,7 +177,7 @@ $(function () {
         $("#tarefa").empty();
         $.getJSON(server + "/tarefas", { usuario: meu_login })
             .done(function (data) {
-                console.log("data: ", data);
+                // console.log("data: ", data);
                 for (var tarefa = 0; tarefa < data.length; tarefa++) {
                     addTarefa(data[tarefa].texto, data[tarefa].id);
                 }
